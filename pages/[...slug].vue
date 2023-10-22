05997abe2215715a2fc5913b6c3ae56659f32335
Aapo Kiiso <aapo@lamia.fi>
2023-10-22 22:53:55 +0200

    Clean up image post page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,10 @@
 <template>
-  <ProseContent>
-    <ContentDoc class="p-6" />
-  </ProseContent>
+  <ContentDoc v-slot="{ doc }">
+    <div v-if="doc.layout === 'image'" class="py-6 px-2 md:px-0">
+      <FeedImageCard :content="doc" />
+    </div>
+    <ProseContent v-else class="p-6">
+      <ContentRenderer :value="doc" />
+    </ProseContent>
+  </ContentDoc>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <ol class="mx-2 md:mx-0">
-    <li v-for="month in months" :key="month" class="my-4">
-      <h2 class="text-lg mb-2 text-center text-gray-500 dark:text-gray-400">
-        {{ formatInDisplayTimeZone(month, 'MMMM y') }}
-      </h2>
+  <div class="px-2 md:px-0">
+    <ol>
+      <li v-for="month in months" :key="month" class="my-4">
+        <h2 class="text-lg mb-2 text-center text-gray-500 dark:text-gray-400">
+          {{ formatInDisplayTimeZone(month, 'MMMM y') }}
+        </h2>
 
-      <ol>
-        <li v-for="item in contentByMonth[month]" :key="item._id" class="my-4">
-          <FeedImageCard v-if="item.layout === 'image'" :content="item" />
-          <FeedTextCard v-if="item.layout === 'text'" :content="item" />
-        </li>
-      </ol>
-    </li>
-  </ol>
+        <ol>
+          <li v-for="item in contentByMonth[month]" :key="item._id" class="my-4">
+            <FeedImageCard v-if="item.layout === 'image'" :content="item" />
+            <FeedTextCard v-if="item.layout === 'text'" :content="item" />
+          </li>
+        </ol>
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script setup lang="ts">
