10f482d7609a675a65f7c20ac436f571773149bf
Aapo Kiiso <aapo@lamia.fi>
2023-11-22 19:58:45 +0100

    Handle page not found

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,10 +1,22 @@
 <template>
-  <ContentDoc v-slot="{ doc }">
-    <div v-if="doc.layout === 'image'" class="py-6 px-2 md:px-0">
-      <FeedImageCard :content="doc" />
-    </div>
-    <ProseContent v-else class="p-6">
-      <ContentRenderer :value="doc" />
-    </ProseContent>
+  <ContentDoc>
+    <template #default="{ doc }">
+      <div v-if="doc.layout === 'image'" class="py-6 px-2 md:px-0">
+        <FeedImageCard :content="doc" />
+      </div>
+      <ProseContent v-else class="p-6">
+        <ContentRenderer :value="doc" />
+      </ProseContent>
+    </template>
+    <template #not-found>
+      <ProseContent class="p-6">
+        <h1>Page not found</h1>
+        <p>
+          <NuxtLink to="/">
+            Back to front page
+          </NuxtLink>
+        </p>
+      </ProseContent>
+    </template>
   </ContentDoc>
 </template>
