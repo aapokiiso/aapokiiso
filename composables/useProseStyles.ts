4a2d188c8eab77660ad4c0f0b71bad0146a8ae34
Aapo Kiiso <aapo@lamia.fi>
2024-02-25 20:16:42 +0100

    Fix styles for heading anchors
    - Remove underline
    - Don't scroll behind fixed menu

--- composables/useProseStyles.ts
+++ composables/useProseStyles.ts
@@ -1,1 +1,1 @@
-export const useProseStyles = (): string => 'prose dark:prose-invert prose-img:rounded-lg'
+export const useProseStyles = (): string => 'prose dark:prose-invert prose-img:rounded-lg prose-headings:scroll-mt-32'
